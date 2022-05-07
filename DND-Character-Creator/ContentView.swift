e36b496b010ee36082ad705b11ebdff8ad275776
Alan Klinect <alanklinect@gmail.com>
2022-05-07 10:46:21 -0500

    Initial Commit

---
+++ DND-Character-Creator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DND-Character-Creator
+//
+//  Created by admin on 5/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DND-Character-Creator/DND_Character_CreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DND_Character_CreatorApp.swift
+//  DND-Character-Creator
+//
+//  Created by admin on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct DND_Character_CreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

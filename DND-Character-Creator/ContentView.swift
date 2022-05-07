93264a59a9d2140309b98f32cbb53632f44bd931
Alan Klinect <alanklinect@gmail.com>
2022-05-07 11:02:19 -0500

    created nice message for teammate

--- DND-Character-Creator/ContentView.swift
+++ DND-Character-Creator/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("You are doing awesome!")
+        Text("You are doing awesome! :D")
             .padding()
     }
 }

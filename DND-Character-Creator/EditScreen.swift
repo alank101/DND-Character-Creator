730c1f909869f64114e3d4af3480f6feae973064
marvin evins <marvinevins@yahoo.com>
2022-05-07 21:35:56 -0500

    Beginning edit screen

--- DND-Character-Creator/EditScreen.swift
+++ DND-Character-Creator/EditScreen.swift
@@ -9,7 +9,38 @@ import SwiftUI
 
 struct EditScreen: View {
     var body: some View {
-        Text("sss")
+        Text("Character Creator")
+        //need same font as initial screej
+        
+        //top part
+        HStack {
+            VStack{
+                // need two drop downs
+                //stacked on top of each other
+            }
+            //need image placement drop
+        }
+        
+        //center section filled with properties of character
+        
+        //only six options so we probably  only need to create just two hstacks
+        // future reference, it we expand this, this could be a lazy grid
+        
+        VStack{
+            HStack{
+                //3 character traits
+            }
+            HStack{
+                //3 character traits
+            }
+        }
+        
+        
+        //NO BOTTOM PART THOUGHT I THINK ONE IS NEEDED
+        
+        
+        
+        
     }
 }
 

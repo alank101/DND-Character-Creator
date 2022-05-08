97dedb01da425e2b7aa1ea9ea712796610cf27aa
marvin evins <marvinevins@yahoo.com>
2022-05-07 23:08:16 -0500

    making folders

---
+++ DND-Character-Creator/components/Dropdown.swift
@@ -0,0 +1,20 @@
+//
+//  Dropdown.swift
+//  DND-Character-Creator
+//
+//  Created by marvin evins on 5/7/22.
+//
+
+import SwiftUI
+
+struct Dropdown: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Dropdown_Previews: PreviewProvider {
+    static var previews: some View {
+        Dropdown()
+    }
+}

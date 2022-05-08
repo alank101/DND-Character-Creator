a282fd1addaca2ac4e3525d2b49e4c30c77c4035
marvin evins <marvinevins@yahoo.com>
2022-05-07 21:19:16 -0500

    Working on initial lead screen splash screen

--- DND-Character-Creator/EditScreen.swift
+++ DND-Character-Creator/EditScreen.swift
@@ -10,10 +10,16 @@ import SwiftUI
 struct EditScreen: View {
     var body: some View {
         ZStack{
-            Image("Dragon")
+            Image("101")
+                .resizable()
+                .aspectRatio (contentMode: .fill)
+                .clipped()
+                .frame(height: 155)
             HStack{
                 //Image 3* dice
+                
             }
+            Spacer()
             Text("Dungeons and Dragons Character Creator")
             //font should look similar to the ui screen on figma mockup
             

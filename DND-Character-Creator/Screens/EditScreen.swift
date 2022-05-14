baba5213f3d19dc0659162ff7025b01030d477db
Alan Klinect <alanklinect@gmail.com>
2022-05-14 11:23:19 -0500

    changed picker location

--- DND-Character-Creator/Screens/EditScreen.swift
+++ DND-Character-Creator/Screens/EditScreen.swift
@@ -33,10 +33,49 @@ struct EditScreen: View {
                     //stacked on top of each other
     //                Dropdown(character: Binding<CharacterModel>)
 //                    Text("\(character.name)")
-                    ClassDropdown(character:  .constant(CharacterModel(name: "", charClass: .barbarian, charRace: .elf, strength: 0, dexterity: 0 , constitution: 0, intelligence: 0, wisdom: 0, charisma: 0, image:Image(systemName: "pencil.circle.fill"))))
+                    Picker(selection: $character.charClass) {
+                        Group {
+                            Text("Sorcerer").tag(Class.sorcerer)
+                            Text("Warlock").tag(Class.warlock)
+                            Text("Fighter").tag(Class.fighter)
+                            Text("Barbarian").tag(Class.barbarian)
+                            Text("Ranger").tag(Class.ranger)
+                            Text("Paladin").tag(Class.paladin)
+                            Text("Bard").tag(Class.bard)
+                            Text("Wizard").tag(Class.wizard)
+                            Text("Cleric").tag(Class.cleric)
+                            Text("Druid").tag(Class.druid)
+                        }
+                        Group {
+                            Text("Rogue").tag(Class.rogue)
+                            Text("Monk").tag(Class.monk)
+                            Text("No Class").tag(Class.no_class)
+                        }
+                    } label: {
+                        Text("Picker")
+                    }.pickerStyle(MenuPickerStyle())
+                        .padding()
+                        .frame(height: 30)
+                        .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color.blue, lineWidth: 1.0).foregroundColor(.gray))
     //                Dropdown(character: Binding<CharacterModel>)
 //                    Text("\(character.name)")
-                    RaceDropdown(character:   .constant(CharacterModel(name: "", charClass: .barbarian, charRace: .elf, strength: 0, dexterity: 0 , constitution: 0, intelligence: 0, wisdom: 0, charisma: 0, image:Image(systemName: "pencil.circle.fill") )))
+                    Picker(selection: $character.charRace) {
+                        Text("Tiefling").tag(Race.tiefling)
+                        Text("Human").tag(Race.human)
+                        Text("Elf").tag(Race.elf)
+                        Text("Half Elf").tag(Race.half_elf)
+                        Text("Halfling").tag(Race.halfling)
+                        Text("Half Orc").tag(Race.half_orc)
+                        Text("Dwarf").tag(Race.dwarf)
+                        Text("Gnome").tag(Race.gnome)
+                        Text("No Race").tag(Race.no_race)
+                    } label: {
+                        Text("Picker")
+                    }.pickerStyle(MenuPickerStyle())
+                        .padding()
+                        .frame(height: 30)
+                        .overlay(RoundedRectangle(cornerRadius: 5).stroke(Color.blue, lineWidth: 1.0).foregroundColor(.gray))
+                    
                     
                     
                 }

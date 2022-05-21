a2a84fdb3d4c11e99015fc16fa248f62458bf5f2
Alan Klinect <alanklinect@gmail.com>
2022-05-21 09:08:04 -0500

    added main screen title

--- DND-Character-Creator/InitialScreenComponents/ContentView.swift
+++ DND-Character-Creator/InitialScreenComponents/ContentView.swift
@@ -43,7 +43,7 @@ struct ContentView: View {
                             Image(systemName: "plus")
                         }
                     }
-            }
+            }.navigationBarTitle("Character List")
         }
     }
 }
